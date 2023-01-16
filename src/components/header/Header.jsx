41a7b5b17452e7c01c915294f9e10ce64dda16c7
davidmedev222 <davidmedev@gmail.com>
2023-01-16 16:40:36 -0300

    feat(mobile): add header interface responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,20 +7,30 @@ const Header = () => {
 
   const { title, navbar, button } = header;
 
-  const navbarLi = navbar.map((cadaLi) => <Li key={cadaLi} text={cadaLi} />); // COMPONENT
+  const navbarLi = navbar.map((cadaLi) => <HeaderLi key={cadaLi} text={cadaLi} />); // COMPONENT
 
   return (
     <header className="header">
+      {/* TITLE */}
       <h2 className="header-title">{title}</h2>
+      {/* BUTTON MENU */}
+      <button className="header-menu">
+        <img
+          src="https://res.cloudinary.com/dos3i5jqy/image/upload/v1673897021/apollonft/main_o9q12x.svg"
+          alt="icono de menu"
+        />
+      </button>
+      {/* NAVBAR */}
       <nav className="header-nav">
         <ul className="header-ul">{navbarLi}</ul>
       </nav>
+      {/* BUTTON */}
       <button className="header-btn">{button}</button>
     </header>
   );
 };
 
-const Li = ({ text }) => {
+const HeaderLi = ({ text }) => {
   return (
     <li className="header-li">
       <Link className="header-a" to={'/'}>
