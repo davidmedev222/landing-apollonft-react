95faa17ecc239446dc3e4ce1c0855765af1b1078
davidmedev222 <davidmedev@gmail.com>
2023-01-16 16:45:21 -0300

    chore(web): add banner interface

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../../context/global/GlobalContext';
+import { Button } from '../button/Button';
+
+const Banner = () => {
+  const { banner } = useContext(GlobalContext);
+
+  const { title, button } = banner;
+
+  return (
+    <section className="banner">
+      {/* TITLE */}
+      <h3 className="banner-title">{title}</h3>
+      {/* BUTTON */}
+      <Button className="banner-btn">{button}</Button>
+    </section>
+  );
+};
+
+export { Banner };
