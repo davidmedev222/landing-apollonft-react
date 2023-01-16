498c84afa391db0e1f2768167285abb0dfd83ede
davidmedev222 <davidmedev@gmail.com>
2023-01-16 16:42:47 -0300

    chore(web): add stats interface

---
+++ src/components/stats/Stats.jsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../../context/global/GlobalContext';
+
+const Stats = () => {
+  const { stats } = useContext(GlobalContext);
+
+  const statsLi = stats.map(({ id, title, stat }) => (
+    <StatsLi key={id} title={title} stat={stat} />
+  )); // COMPONENT
+
+  return <ul className="stats">{statsLi}</ul>;
+};
+
+const StatsLi = ({ title, stat }) => {
+  return (
+    <li className="stats-li">
+      <span className="stats-title">{title}</span>
+      <span className="stats-stat">{stat}</span>
+    </li>
+  );
+};
+
+export { Stats };
